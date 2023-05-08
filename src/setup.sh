293d5cfb4a87f1c6ef9f21587b0c9bc3e9c7c1a1
huncfut <k.zeligowski@gmail.com>
2023-05-08 17:52:22 +0200

    Server starts, but is not stopped by the lazymc

--- src/setup.sh
+++ src/setup.sh
@@ -6,12 +6,16 @@ MINECRAFT_VERSION=1.19.2
 SERVER_DIR=server
 LAZYMC_VERSION=0.2.10
 FORGE_VERSION=43.2.8
+PROTOCOL=760
+EULA=true
 
 ARCH=$(arch)
 if [ "$ARCH" == "x86_64" ]; then ARCH=x64; fi
 
 LAZYMC_DOWNLOAD_LINK=https://github.com/timvisee/lazymc/releases/download/v${LAZYMC_VERSION}/lazymc-v${LAZYMC_VERSION}-linux-${ARCH}
 LAZYMC_PATH=lazymc
+LAZYMC_CONFIG_PATH=${LAZYMC_PATH}.toml
+START_SERVER_PATH=${SERVER_DIR}/start_server.sh
 
 FORGE_DOWNLOAD_LINK=https://maven.minecraftforge.net/net/minecraftforge/forge/${MINECRAFT_VERSION}-${FORGE_VERSION}/forge-${MINECRAFT_VERSION}-${FORGE_VERSION}-installer.jar
 FORGE_INSTALLER_PATH=installer.jar
@@ -54,9 +58,42 @@ function download_lazymc {
     else 
       echo "Lazymc download failed. Abort..."
       exit 10
-fi
+  fi
 }
 
+# ========= CONFIG LAZYMC =========
+function config_lazymc {
+  # -- PUBLIC
+  echo "[public]" > ${LAZYMC_CONFIG_PATH}
+  echo "version = \"${MINECRAFT_VERSION}\"" >> ${LAZYMC_CONFIG_PATH}
+  echo "protocol = ${PROTOCOL}" >> ${LAZYMC_CONFIG_PATH}
+
+  # -- SERVER
+  echo "[server]" >> ${LAZYMC_CONFIG_PATH}
+  echo "directory = \"${SERVER_DIR}\"" >> ${LAZYMC_CONFIG_PATH}
+  echo "command = \"bash start_server.sh\"" >> ${LAZYMC_CONFIG_PATH}
+  echo "forge = true" >> ${LAZYMC_CONFIG_PATH}
+
+  # -- MOTD
+  echo "[motd]" >> ${LAZYMC_CONFIG_PATH}
+  echo "sleeping = \"☠ Server is sleeping\n§2☻ Join to start it up\"" >> ${LAZYMC_CONFIG_PATH}
+  echo "starting = \"§2☻ Server is starting...\n§7⌛ Please wait...\"" >> ${LAZYMC_CONFIG_PATH}
+  echo "stopping = \"☠ Server going to sleep...\n⌛ Please wait...\"" >> ${LAZYMC_CONFIG_PATH}
+
+  # -- CONFIG
+  echo "[config]" >> ${LAZYMC_CONFIG_PATH}
+  echo "version = \"${LAZYMC_VERSION}\"" >> ${LAZYMC_CONFIG_PATH}
+}
+
+# ========= CREATE STARTING SCRIPT =========
+function create_starting_script {
+  echo "trap 'kill -TERM \$PID' TERM INT" > ${START_SERVER_PATH}
+  cat ${SERVER_DIR}/run.sh | grep -v ^# | sed "s/\".*/--nogui \&/" >> ${START_SERVER_PATH}
+  echo 'PID=$!' >> ${START_SERVER_PATH}
+  echo 'wait $PID' >> ${START_SERVER_PATH}
+  echo 'trap - TERM INT' >> ${START_SERVER_PATH}
+  echo 'wait $PID' >> ${START_SERVER_PATH}
+}
 
 # ┏━━━━━━━━━━━━━━━━━┓
 # ┃ MINECRAFT FORGE ┃
@@ -66,7 +103,6 @@ function download_forge {
   if wget ${FORGE_DOWNLOAD_LINK} -q --show-progress -O ${FORGE_INSTALLER_PATH}
     then 
       echo "Forge downloaded sucessfully"
-      chmod a+x ${LAZYMC_PATH}
     else 
       echo "Forge download failed. Abort..."
       exit 20
@@ -97,17 +133,9 @@ function install_forge {
 # ┏━━━━━━━━━━━━━━━━━━━┓
 # ┃ MAIN SETUP SCRIPT ┃
 # ┗━━━━━━━━━━━━━━━━━━━┛
-echo ${ARCH}
 echo -e "${S_H1}LAZYFORGE-DOCKER STARTING UP!!!${S_R}"
 
-echo -e "\n${S_H2}CHECK LAZYMC${S_R}"
-if [ -f "${LAZYMC_PATH}" ]
-  then
-    echo "Lazymc installed"
-  else
-    echo "Lazymc not detected. Downloading Lazymc"
-    download_lazymc
-fi
+# -- FORGE
 echo -e "\n${S_H2}CHECK FORGE${S_R}"
 if [ -f "${SERVER_DIR}/run.sh" ]
   then
@@ -116,7 +144,33 @@ if [ -f "${SERVER_DIR}/run.sh" ]
     echo "No run.sh script detected. Installing Forge"
     download_forge
     install_forge
+    echo "eula=${EULA}" > ${SERVER_DIR}/eula.txt
+    touch ${SERVER_DIR}/server.properties
+fi
+
+# -- LAZYMC
+echo -e "\n${S_H2}CHECK LAZYMC${S_R}"
+if [ -f "${LAZYMC_PATH}" ]
+  then
+    echo "Lazymc installed"
+  else
+    echo "Lazymc not detected. Downloading Lazymc"
+    download_lazymc
+fi
+if [ -f "${LAZYMC_CONFIG_PATH}" ]
+  then
+    echo "Lazymc config found"
+  else
+    echo "Creating lazymc config"
+    config_lazymc
+fi
+if [ -f "${START_SERVER_PATH}" ]
+  then
+    echo "Starting script found"
+  else
+    echo "Creating startup script"
+    create_starting_script
 fi
 
 echo "Starting UP!"
-exec ./${LAZYMC_PATH}
+exec ./lazymc
